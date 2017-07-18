bd4198fb1ceac69b0f0dc3a16a4ddea6f73ff847
Amulya Gaur <amulyagaur111@gmail.com>
2017-07-18 19:14:35 +0530

    Create EIGHTS.cpp

---
+++ EIGHTS.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    int t;
+    cin>>t;
+    for(int z=1;z<=t;z++)
+    {
+      long long k;
+      cin>>k;
+      cout<<192+(k-1)*250<<endl;
+    }
+    return 0;
+}
