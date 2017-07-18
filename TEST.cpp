b0adf3315a2b30a6f9eb16f1bda44d22c3a30f09
Amulya Gaur <amulyagaur111@gmail.com>
2017-07-18 15:56:29 +0530

    Create TEST.cpp

---
+++ TEST.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	// your code goes here
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    while(1)
+    {
+    	int d;
+    	cin>>d;
+    	if(d==42)
+    	break;
+    	else
+    	cout<<d<<endl;
+    }
+	return 0;
+}
